8c93c5a504f8a001422eddc2a0ad9eaa1ec8fd51
kamarmohsin <mohsin.saphana@gmail.com>
2017-07-29 09:21:53 +0530

    Update db_test_calcu_V1.js

--- db_test_calcu_V1.js
+++ db_test_calcu_V1.js
@@ -233,7 +233,14 @@ var $mj = jQuery.noConflict();
 					CTCcalculation['benifit_entertainment_an'] = isNaN(parseFloat($mj('[name="benifit_entertainment_an"]').val()))? 0 : parseFloat($mj('[name="benifit_entertainment_an"]').val());
 					CTCcalculation['benifit_entertainment_mon'] = CTCcalculation['benifit_entertainment_an']/12;
 					
-					
+					CTCcalculation['total_D_an'] = CTCcalculation['benifit_driversalary_an'] + CTCcalculation['benifit_mobTel_an'] + CTCcalculation['benifit_additionalMob_an'] + CTCcalculation['benifit_residenceTel_an'] + CTCcalculation['benifit_airFare_an'] + CTCcalculation['benifit_entertainment_an'];
+					CTCcalculation['total_D_mon'] = CTCcalculation['total_D_an'] /12;
+					CTCcalculation['total_D1D1_an'] = CTCcalculation['benifit_PLI_an'] + CTCcalculation['total_D_an'];
+					CTCcalculation['total_D1D1_mon'] = CTCcalculation['total_D1D1_an'] /12;
+					CTCcalculation['total_CTC_an'] = inputCTC_an;
+					CTCcalculation['total_CTC_mon'] = CTCcalculation['total_CTC_an'] /12;
+					CTCcalculation['total_CTCPLI_an'] = CTCcalculation['total_CTC_an'] + CTCcalculation['benifit_PLI_an'];
+					CTCcalculation['total_CTCPLI_mon'] = CTCcalculation['total_CTCPLI_an'] /12;
 					
 
  					

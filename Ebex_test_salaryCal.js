2bb9f6aaccc54b7f687367b79a7c5e3ffc492148
kamarmohsin <mohsin.saphana@gmail.com>
2017-08-23 11:15:14 +0530

    Update Ebex_test_salaryCal.js

--- Ebex_test_salaryCal.js
+++ Ebex_test_salaryCal.js
@@ -124,10 +124,10 @@ var $mj = jQuery.noConflict();
 
 				
 
-					var variablePay_mon = isNaN(($mj('[name="eb_variablePay_mon"]').val()))? parseFloat(paygrade[grade]) : parseFloat($mj('[name="eb_variablePay_mon"]').val());
+					var variablePay_mon = isNaN(($mj('[name="eb_variablePay_mon"]').val()))? paygrade[grade] : parseFloat($mj('[name="eb_variablePay_mon"]').val());
                                         
 
-					CTCcalculation['eb_variablePay_mon'] = variablePay_mon;
+					CTCcalculation['eb_variablePay_mon'] = parseFloat(variablePay_mon);
 					
 					CTCcalculation['eb_variablePay_an'] = CTCcalculation['eb_variablePay_mon'] * 12;
 					CTCcalculation['eb_CTC_mon'] = inputeb_CTC_mon - CTCcalculation['eb_variablePay_mon'];

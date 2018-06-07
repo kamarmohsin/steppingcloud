f0d858e5fee6ac9bfc9d8982f249ba5bff7c7497
kamarmohsin <mohsin.saphana@gmail.com>
2018-06-08 02:35:28 +0530

    Update Cipla_saga_v1.js

--- Cipla_saga_v1.js
+++ Cipla_saga_v1.js
@@ -53,6 +53,10 @@ return new Za.prototype.init(a,b,c,d,e)}m.Tween=Za,Za.prototype={constructor:Za,
 				CTCComponents['disability'] = (basic * 0.95)/100;
 				CTCComponents['admedGap'] = 49.50;
 				CTCComponents['funeral'] = 16.05;
+					
+				// subtotal_compContribution
+				
+				CTCComponents['subtotal_compContribution'] = CTCComponents['groupLife'] + CTCComponents['disability'] +	CTCComponents['admedGap'] + CTCComponents['funeral'] + vitality;
 				CTCComponents['pensionFund'] = (basic * pf_manfct)/100;
 				//CTCComponents['medicalAid'] = 2109.50;
 				CTCComponents['ded_admedGap'] = CTCComponents['admedGap'];
@@ -162,6 +166,8 @@ return new Za.prototype.init(a,b,c,d,e)}m.Tween=Za,Za.prototype={constructor:Za,
 				CTCComponents['disability'] = (basic * 0.56)/100;
 				CTCComponents['admedGap'] = 49.50;
 				CTCComponents['funeral'] = 16.05;
+				
+				CTCComponents['subtotal_compContribution'] = CTCComponents['groupLife'] + CTCComponents['disability'] +	CTCComponents['admedGap'] + CTCComponents['funeral'] + vitality;	
 				CTCComponents['pensionFund'] = (basic * 7.5)/100;
 				//CTCComponents['medicalAid'] = 2109.50;
 				CTCComponents['ded_admedGap'] = CTCComponents['admedGap'];

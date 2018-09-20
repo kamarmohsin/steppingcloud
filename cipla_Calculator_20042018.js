c1faa075b9a8b21ee9816c0c5a0bcaa6fec8b15a
kamarmohsin <mohsin.saphana@gmail.com>
2018-09-20 12:32:22 +0530

    Update cipla_Calculator_20042018.js

--- cipla_Calculator_20042018.js
+++ cipla_Calculator_20042018.js
@@ -583,7 +583,9 @@ return new Za.prototype.init(a,b,c,d,e)}m.Tween=Za,Za.prototype={constructor:Za,
 							'performaneBonus_mon':0,
 							'performanceBonus_an':0,
 							'totalC_mon':0,
-							'totalC_an':0
+							'totalC_an':0,
+							'basketAllowance_an':0,
+							'basketAllowance_mon':0
 
 
 						}
@@ -2290,6 +2292,30 @@ return new Za.prototype.init(a,b,c,d,e)}m.Tween=Za,Za.prototype={constructor:Za,
 						CTCComponents['totalB_mon'] = CTCComponents['totalA_mon'] + CTCComponents['providentFundMonth'] + CTCComponents['exgratia2_mon'];
 						CTCComponents['totalB_an'] = CTCComponents['totalB_mon'] * 12; 
 					}
+					else if(loc == 'goa' && qualf == 'Bsc202320' && design =='trainee' ){
+
+						CTCComponents['BasicSal_mon'] = 8000;
+						CTCComponents['BasicSal_an'] = CTCComponents['BasicSal_mon'] *12;
+						CTCComponents['housingAlMonthly'] = 2100;
+						CTCComponents['housingAllowance_an'] = CTCComponents['housingAlMonthly'] *12;
+						
+						CTCComponents['foodAllowance_mon'] = 600;
+						CTCComponents['foodAllowance_an'] = CTCComponents['foodAllowance_mon'] *12;
+						CTCComponents['conveyance_mon'] = 1300;
+						CTCComponents['conveyance_an'] = CTCComponents['conveyance_mon'] * 12;
+						CTCComponents['additionalAllow_mon'] = 2500;
+						CTCComponents['additionalAllow_an'] = CTCComponents['additionalAllow_mon'] * 12;
+						//CTCComponents['RLA_mon'] = 4500;
+						CTCComponents['totalA_mon'] = CTCComponents['BasicSal_mon']+ CTCComponents['housingAlMonthly'] + CTCComponents['foodAllowance_mon'] + CTCComponents['conveyance_mon'] + CTCComponents['additionalAllow_mon']  ;
+						CTCComponents['totalA_an'] = CTCComponents['totalA_mon'] * 12;
+						CTCComponents['providentFundMonth'] = 960;
+						CTCComponents['providentFund_an'] = CTCComponents['providentFundMonth'] * 12;
+						CTCComponents['exgratia2_mon'] = 1400;
+						CTCComponents['exgratia2_ann'] = CTCComponents['exgratia2_mon'] *12;
+						CTCComponents['totalB_mon'] = CTCComponents['totalA_mon'] + CTCComponents['providentFundMonth'] + CTCComponents['exgratia2_mon'];
+						CTCComponents['totalB_an'] = CTCComponents['totalB_mon'] * 12;
+					}
+
 
 
 					
@@ -2698,6 +2724,7 @@ return new Za.prototype.init(a,b,c,d,e)}m.Tween=Za,Za.prototype={constructor:Za,
 				        $mj('#qualification').append('<option value="dipPharma">Diploma Pharma</option>');
 				        $mj('#qualification').append('<option value="BscBaBcom">B.sc/B.A/B.Com</option>');
 				        $mj('#qualification').append('<option value="BscPgdca">B.sc+PGDCA</option>');
+				        $mj('#qualification').append('<option value="Bsc202320">B.sc(202320)</option>'); 
 				        $mj('#qualification').append('<option value="gradBPharmacist">Graduate Pharmacist(B Pharma)</option>');
 				        $mj('#qualification').append('<option value="msc">M.sc</option>');
 				        $mj('#qualification').append('<option value="gradEngineer">Graduate Engineer(BE/ B.Tech/ B.Sc (Tech) )</option>');
